2d40d79a86d27bcc92590131bb5bd5406598319f
katerina <klarina2409@gmail.com>
2024-07-26 12:23:27 +0300

    Ready2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Исходные данные о морковке на полянках
+        Map<Integer, List<Integer>> carrotsByField = new HashMap<>();
+        carrotsByField.put(1, Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+        carrotsByField.put(2, Arrays.asList(2, 2, 2, 2, 2, 2, 2, 2, 2, 2));
+        carrotsByField.put(3, Arrays.asList(3, 3, 3, 3, 3, 3, 3, 3, 3, 3));
+        carrotsByField.put(4, Arrays.asList(4, 4, 4, 4, 4, 4, 4, 4, 4, 4));
+        carrotsByField.put(5, Arrays.asList(5, 5, 5, 5, 5, 5, 5, 5, 5, 5));
+
+        // Объединяем все морковки в один список
         List<Integer> carrots = new ArrayList<>();
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 0; j < 10; j++) {
-                carrots.add(i);
-            }
+        for (List<Integer> fieldCarrots : carrotsByField.values()) {
+            carrots.addAll(fieldCarrots);
         }
 
         // Сортировка морковки по убыванию веса
@@ -23,16 +33,14 @@ public class Main {
             int currentWeight = 0;
             List<Integer> selectedCarrots = new ArrayList<>();
 
-            for (int i = 0; i < carrots.size(); i++) {
-                if (currentWeight + carrots.get(i) <= maxWeightPerMove) {
-                    currentWeight += carrots.get(i);
-                    selectedCarrots.add(carrots.get(i));
+            Iterator<Integer> iterator = carrots.iterator();
+            while (iterator.hasNext()) {
+                int carrot = iterator.next();
+                if (currentWeight + carrot <= maxWeightPerMove) {
+                    currentWeight += carrot;
+                    selectedCarrots.add(carrot);
+                    iterator.remove();  // Удаляем морковку сразу после выбора
                 }
-            }
-
-            // Удаляем выбранную морковку из списка
-            for (Integer carrot : selectedCarrots) {
-                carrots.remove(carrot);
             }
 
             totalCarrotsCarried += currentWeight;
